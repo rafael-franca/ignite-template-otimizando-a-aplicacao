3acaa74bba23210db903fda4273d91935a351863
Rafael França <rafael.franca@ibyte.com.br>
2022-02-01 21:00:37 -0300

    [feat] Improve performance of loading

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,9 @@
+import type { ButtonHTMLAttributes } from 'react';
+
+import { memo } from 'react';
 import { Icon } from './Icon';
 
 import '../styles/button.scss';
-import { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
@@ -9,7 +11,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   selected: boolean;
 }
 
-export function Button({ iconName, title, selected, ...rest }: ButtonProps) {
+function ButtonComponent({ iconName, title, selected, ...rest }: ButtonProps) {
   return (
     <button type="button" {...(selected && { className: 'selected' })} {...rest}>
       <Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
@@ -17,3 +19,7 @@ export function Button({ iconName, title, selected, ...rest }: ButtonProps) {
     </button>
   );
 }
+
+export const Button = memo(ButtonComponent, (prevProps, nextProps) => {
+  return prevProps.selected == nextProps.selected
+})
